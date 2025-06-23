ac3d00619160b3290943649ea905b14ff1f833f7
seamme <seamhack@gmail.com>
2025-06-23 21:47:17 +0700

    Add new files

---
+++ src/app/(admin)/(others-pages)/(tables)/leads-tables/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+import ComponentCard from "@/components/common/ComponentCard";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import LeadsTable from "@/components/tables/LeadsTable";
+import { Metadata } from "next";
+import React from "react";
+import Button from "@/components/ui/button/Button";
+import Input from "@/components/form/input/InputField";
+import Label from "@/components/form/Label";
+import { useModal } from "@/hooks/useModal";
+import { Modal } from "@/components/ui/modal";
+
+// export const metadata: Metadata = {
+//   title: "Next.js Basic Table | TailAdmin - Next.js Dashboard Template",
+//   description:
+//     "This is Next.js Basic Table  page for TailAdmin  Tailwind CSS Admin Dashboard Template",
+//   // other metadata
+// };
+
+export default function LeadsTables() {
+    const { isOpen, openModal, closeModal } = useModal();
+    const handleSave = () => {
+      // Handle save logic here
+      console.log("Saving changes...");
+      closeModal();
+    };
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Leads Table" />
+      <div className="space-y-6">
+        <ComponentCard title="Leads Table">
+          <div className="flex items-center gap-3">
+            <Button size="md" variant="primary"
+              onClick={openModal}
+            >
+              Add Lead +
+            </Button>
+            <Button size="md" variant="outline">
+              Edit Lead
+            </Button>
+            </div>
+          <LeadsTable />
+        </ComponentCard>
+      </div>
+
+      <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[700px] m-4">
+        <div className="no-scrollbar relative w-full max-w-[700px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
+          <div className="px-2 pr-14">
+            <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
+              Edit Personal Information
+            </h4>
+            <p className="mb-6 text-sm text-gray-500 dark:text-gray-400 lg:mb-7">
+              Update your details to keep your profile up-to-date.
+            </p>
+          </div>
+          <form className="flex flex-col">
+            <div className="custom-scrollbar h-[450px] overflow-y-auto px-2 pb-3">
+              <div>
+                <h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
+                  Social Links
+                </h5>
+
+                <div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
+                  <div>
+                    <Label>Facebook</Label>
+                    <Input
+                      type="text"
+                      defaultValue="https://www.facebook.com/PimjoHQ"
+                    />
+                  </div>
+
+                  <div>
+                    <Label>X.com</Label>
+                    <Input type="text" defaultValue="https://x.com/PimjoHQ" />
+                  </div>
+
+                  <div>
+                    <Label>Linkedin</Label>
+                    <Input
+                      type="text"
+                      defaultValue="https://www.linkedin.com/company/pimjo"
+                    />
+                  </div>
+
+                  <div>
+                    <Label>Instagram</Label>
+                    <Input
+                      type="text"
+                      defaultValue="https://instagram.com/PimjoHQ"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-7">
+                <h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
+                  Personal Information
+                </h5>
+
+                <div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>First Name</Label>
+                    <Input type="text" defaultValue="Musharof" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Last Name</Label>
+                    <Input type="text" defaultValue="Chowdhury" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Email Address</Label>
+                    <Input type="text" defaultValue="randomuser@pimjo.com" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Phone</Label>
+                    <Input type="text" defaultValue="+09 363 398 46" />
+                  </div>
+
+                  <div className="col-span-2">
+                    <Label>Bio</Label>
+                    <Input type="text" defaultValue="Team Manager" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
+              <Button size="sm" variant="outline" onClick={closeModal}>
+                Close
+              </Button>
+              <Button size="sm" onClick={handleSave}>
+                Save Changes
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+    </div>
+    
+  );
+}
