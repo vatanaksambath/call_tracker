788c6e9953e9c46c41b38f22f388ec7c1fa2282d
vatanak <vatanak.sambath@canadiabank.com.kh>
2025-07-16 17:02:39 +0700

    feat: update web to PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,22 @@ import './globals.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
+import type { Metadata, Viewport } from 'next';
 
 const outfit = Outfit({
   subsets: ["latin"],
 });
+
+export const viewport: Viewport = {
+  themeColor: '#000000',
+};
+
+
+export const metadata: Metadata = {
+  title: 'Call Tracker',
+  description: 'YCall Tracker Application',
+  manifest: '/manifest.json',
+};
 
 export default function RootLayout({
   children,
